a538ce4b1c590328f36eaca58b142c62ce616dae
Yoliser <nethlarion@live.fr>
2020-07-27 15:04:01 +0200

    [IMPROVE_LIST_PHOTOS] Adapt number of photos by row using FlexBoxLayout library

--- app/src/main/java/com/exomind/technical_test/ui/photos_list/PhotosListFragment.kt
+++ app/src/main/java/com/exomind/technical_test/ui/photos_list/PhotosListFragment.kt
@@ -7,12 +7,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.exomind.technical_test.R
 import com.exomind.technical_test.domain.model.Either
 import com.exomind.technical_test.ui.common.BaseFragment
 import com.exomind.technical_test.ui.photos_list.view.adapter.PhotosListAdapter
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import kotlinx.android.synthetic.main.fragment_photos_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
@@ -36,7 +37,11 @@ class PhotosListFragment : BaseFragment(true) {
 
         Timber.d("Will show photos for album ${navArgs.selectedAlbumUi.title} [${navArgs.selectedAlbumUi.id}]")
         fragment_photos_list_recycler_view.apply {
-            layoutManager = GridLayoutManager(context, 2, LinearLayoutManager.VERTICAL, false)
+            val flexboxManager = FlexboxLayoutManager(context).apply {
+                flexDirection = FlexDirection.ROW
+                justifyContent = JustifyContent.SPACE_EVENLY
+            }
+            layoutManager = flexboxManager// GridLayoutManager(context, 2, LinearLayoutManager.VERTICAL, false)
             adapter = photosListAdapter
         }
 
